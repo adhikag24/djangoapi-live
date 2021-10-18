f3decb197c1fcf50b05b42cd463597a914e16ff3
everythinggoeswell <55830734+adhikag24@users.noreply.github.com>
2021-10-18 12:42:08 +0700

    Update settings.py

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'djangoapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        'NAME':    os.environ.get("SQL_DATABASE", BASE_DIR + "db.sqlite3"),
-        'USER': os.environ.get("SQL_USER", "user"),
-        'PASSWORD': os.environ.get("SQL_PASSWORD", "password"),
-        'HOST': os.environ.get("SQL_HOST", "localhost"),
-        "PORT":  os.environ.get("SQL_PORT", "5432"),
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME':    "wqetunxe",
+        'USER': "wqetunxe",
+        'PASSWORD': "Jl3kqFDZWtoG6rLZ7DF_k9Wd6GGa3Jt_",
+        'HOST': "rosie.db.elephantsql.com",
+        "PORT":  "5432",
 
     }
 }
