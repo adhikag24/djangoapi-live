c387b47607147a6d52f7c7ae1a8a2acd2f54da44
everythinggoeswell <55830734+adhikag24@users.noreply.github.com>
2021-10-18 12:37:33 +0700

    Update settings.py

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r^q-t(nnu5)%(fjto1a3h#m__^)mu^5@ydpc+-b#xs#t-ofwd='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://djangoapiadhika.herokuapp.com', '127.0.0.1']
 
